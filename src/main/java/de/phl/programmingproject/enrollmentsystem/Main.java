0ab2b20a17c2ba5868becf8f90ee2aa2f82d32dd
HeroldderBringer <sephiroth2014@gmx.de>
2025-11-05 22:52:48 +0100

    Übung 1 und 2 bearbeitet

--- src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
+++ src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
@@ -5,6 +5,32 @@ package de.phl.programmingproject.enrollmentsystem;
  */
 public class Main {
     public static void main(String[] args) {
-        // TODO: implement your program here.
+        Student john = new Student("John Doe", "12345");                            // Aufgabe 1
+
+        Course computerScience = new Course("Introduction to Computer Science");       // Aufgabe 2
+
+        System.out.println(john.getInfo() + computerScience.getInfo());                      // Aufgabe 3
+
+        Enrollment johnComputerScienceEnrollment = john.enroll(computerScience);             // Aufgabe 4
+
+        System.out.println(john.getInfo() + computerScience.getInfo());                      // Aufgabe 5
+
+        System.out.println(johnComputerScienceEnrollment.getInfo());                         // Aufgabe 6
+
+        johnComputerScienceEnrollment.setGrade(4.0);                                  // Aufgabe 7
+
+        System.out.println(johnComputerScienceEnrollment.getInfo());                         // Aufgabe 8
+
+        Course oop = new Course("Object-Oriented Programming");                        // Aufgabe 9
+
+        john.enroll(oop);                                                                    // Aufgabe 10
+
+        System.out.println(john.getCourses());                                               // Aufgabe 11
+
+        System.out.println(computerScience.getStudents());                                   // Aufgabe 12
+
+        john.drop(computerScience);                                                          // Aufgabe 13
+
+        System.out.println(computerScience.getStudents());                                   // Aufgabe 14
     }
 }
