e207818c6666777106988a98b395dbb06789149a
Kemal Cevik <cevikk859@gmail.com>
2025-11-08 01:36:05 +0300

    übung 1 bearbeitet

--- src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
+++ src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
@@ -1,7 +1,7 @@
 package de.phl.programmingproject.enrollmentsystem;
 
 /**
- * PUT YOUR CODE FOR THE EXERCISE 'STUDENT ENROLLMENT SYSTEM' IN THIS CLASS
+ * 
  */
 public class Main {
     public static void main(String[] args) {
--- src/main/java/de/phl/programmingproject/petadoption/Main.java
+++ src/main/java/de/phl/programmingproject/petadoption/Main.java
@@ -1,26 +1,27 @@
+
+
 package de.phl.programmingproject.petadoption;
 
 import java.util.Date;
 
-/**
- * PUT YOUR CODE FOR THE EXERCISE 'Pet Adoption Center' HERE.
- */
+
+ 
+ 
 public class Main {
     public static void main(String[] args) {
-        Pet buddy = new Pet("Buddy", 3, "Labrador Retriever");                                        // Aufgabe 1
-                                                                            
-        Adopter jane = new Adopter("Jane Smith", "555-555-5555", "janesmith@email.com");            // Aufgabe 2
+        Pet buddy = new Pet("Buddy", 3, "Labrador Retriever");                                                                                                               
+        Adopter jane = new Adopter("Jane Smith", "555-555-5555", "janesmith@email.com");            
 
-        System.out.println(buddy.getInfo());                                                                          // Aufgabe 3
+        System.out.println(buddy.getInfo());                                                                          
 
-        System.out.println(jane.getInfo());                                                                           // Aufgabe 4
+        System.out.println(jane.getInfo());                                                                          
 
-        System.out.println(buddy.isAdopted());                                                                        // Aufgabe 5
+        System.out.println(buddy.isAdopted());                                                                       
 
-        Adoption adoption = buddy.adopt(jane, new Date());                                                            // Aufgabe 6
+        Adoption adoption = buddy.adopt(jane, new Date());                                                            
 
-        System.out.println(adoption.getInfo());                                                                       // Aufgabe 7
+        System.out.println(adoption.getInfo());                                                                       
 
-        System.out.println(buddy.isAdopted());                                                                        // Aufgabe 8
+        System.out.println(buddy.isAdopted());                                                                        
     }
 }
