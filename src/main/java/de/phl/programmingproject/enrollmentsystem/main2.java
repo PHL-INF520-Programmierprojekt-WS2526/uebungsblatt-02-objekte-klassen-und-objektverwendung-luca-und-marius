6337524f091f8cfc61ae9eb58f9f6c48640b9168
Kemal Cevik <cevikk859@gmail.com>
2025-11-11 01:38:23 +0300

    Letzte Bearbeitung auf enrollmentsystem

---
+++ src/main/java/de/phl/programmingproject/enrollmentsystem/main2.java
@@ -0,0 +1,34 @@
+package de.phl.programmingproject.enrollmentsystem;
+
+public class main2 {
+
+    public static void main(String[] args) {
+        Student john = new Student("John Doe", "12345");
+       
+        Course computerScience = new Course("Introduction to Computer Science");
+      
+        System.out.println(john.getInfo() + computerScience.getInfo());
+       
+        Enrollment johnComputerScienceEnrollment = john.enroll(computerScience);
+       
+        System.out.println(john.getInfo() + computerScience.getInfo());
+       
+        System.out.println(johnComputerScienceEnrollment.getInfo());
+       
+        johnComputerScienceEnrollment.setGrade(4.0);
+       
+        System.out.println(johnComputerScienceEnrollment.getInfo());
+       
+        Course oop = new Course("Object-Oriented Programming");
+        john.enroll(oop);
+       
+        System.out.println(john.getCourses());
+       
+        System.out.println(computerScience.getStudents());
+       
+        john.drop(computerScience);
+       
+        System.out.println(computerScience.getStudents());
+    }
+
+}
